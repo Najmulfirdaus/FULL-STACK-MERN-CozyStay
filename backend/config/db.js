254dc773975ce46d0d7693dc6d8b60de4d741717
vicknot786@gmail.com <vicknot786@gmail.com>
2026-06-23 13:42:37 +0530

    Fix DB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.MONGODB_URL)
         console.log("DB connected")
     } catch (error) {
-        console.log("db error")
+        console.log("db error",error.message)
     }
 }
 export default connectDb
