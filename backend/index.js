3c919ba3e7de1a632382d650692a06250f638981
vicknot786@gmail.com <vicknot786@gmail.com>
2026-06-23 14:10:57 +0530

    Fix CORS for Render frontend

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,12 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+  origin: [
+    "http://localhost:5173",
+    "https://cozy-stay-f3lc.onrender.com"
+  ],
+  credentials: true
 }))
-
 app.use("/api/auth", authRouter )
 app.use("/api/user", userRouter )
 app.use("/api/listing",listingRouter )
